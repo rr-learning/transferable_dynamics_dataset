cc2866e445e50b39c217643f4473b0ba93da2619
Diego Agudelo-Espana <dagudelo@tue.mpg.de>
2019-05-16 12:07:30 +0200

    Refactoring the unrolling of the dataset and the method to merge in a standard way the dynamics inputs in such a way that it takes advantage of the vectorization and therefore can handle multiple instances at the same time

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -4,12 +4,7 @@ for different learning algorithms
 """
 import os
 import pickle
-
-
 import numpy as np
-
-
-from DL.dynamics_learner_interface.dynamics_learner_interface import DynamicsLearnerInterface
 
 
 def loadRobotData(filename):
@@ -31,13 +26,40 @@ def loadRobotData(filename):
     return observations, actions
 
 def concatenateActionsStates(history_actions, history_obs, future_actions):
+    assert len(history_actions.shape) == 3
+    assert len(history_obs.shape) == 3
+    assert len(future_actions.shape) == 3
+    assert history_actions.shape[:2] == history_obs.shape[:2]
+    assert (history_actions.shape[0], history_actions.shape[2]) ==\
+            (future_actions.shape[0], future_actions.shape[2])
     """
-    concatenates observations and actions to form a single vector. This function
-    is intended to standardize the order observations and actions are merged to
-    form the dynamics input.
+    concatenates observations and actions to form a single (row) vector. This
+    function is intended to standardize the order observations and actions are
+    merged to form the dynamics input. Note that it can handle multiple
+    sequences at the same time.
+
+    Parameters
+    ----------
+
+    history_actions:    np array with shape nsequences x history len x input dim
+    history_obs:        np array with shape nsequences x history len x state dim
+    future_actions:     np array with shape nsequences x prediction horizon - 1
+                        x input dim.
+    Returns
+    -------
+
+    joint_states_actions: np array with shape nsequences x (history len x input dim
+                          + history len x state dim + (prediction horizon - 1)
+                          * input_dim).
+
     """
-    return np.concatenate((history_actions.flatten(), history_obs.flatten(),
-            future_actions.flatten()))
+    joint_states_actions = [history_actions.reshape(
+        (history_actions.shape[0], -1)), history_obs.reshape(
+        (history_obs.shape[0], -1)), future_actions.reshape(
+        (future_actions.shape[0], -1))]
+    joint_states_actions = np.hstack(joint_states_actions)
+    assert joint_states_actions.shape[0] == history_obs.shape[0]
+    return joint_states_actions
 
 def unrollTrainingData(obs_seqs, actions_seqs, history_len, prediction_horizon):
     """
@@ -51,114 +73,18 @@ def unrollTrainingData(obs_seqs, actions_seqs, history_len, prediction_horizon):
     inputs:    np-array of shape traininig_instances x input_dimension
                Note that input_dimension = (action dim+state dim)*history_len
     """
+    assert obs_seqs.shape[:2] == actions_seqs.shape[:2]
     inputs = []
     targets = []
-    for obs, act in zip(obs_seqs, actions_seqs):
-        length = obs.shape[0]
-        for offset in range(history_len, length - prediction_horizon + 1):
-            hist_obs = obs[offset - history_len:offset]
-            hist_act = act[offset - history_len:offset]
-            future_act = act[offset: offset + prediction_horizon - 1]
-            output_obs = obs[offset + prediction_horizon - 1]
-            current_input = concatenateActionsStates(hist_act, hist_obs,
-                    future_act)
-            current_target = output_obs
-            inputs.append(current_input)
-            targets.append(current_target)
+    nrollouts, length, nstates = obs_seqs.shape
+    for offset in range(history_len, length - prediction_horizon + 1):
+        hist_obs = obs_seqs[:, offset - history_len:offset, :]
+        hist_act = actions_seqs[:, offset - history_len:offset, :]
+        future_act = actions_seqs[:,offset: offset + prediction_horizon - 1, :]
+        output_obs = obs_seqs[:,offset + prediction_horizon - 1, :]
+        current_input = concatenateActionsStates(hist_act, hist_obs,
+                future_act)
+        current_target = output_obs
+        inputs.append(current_input)
+        targets.append(current_target)
     return np.vstack(targets), np.vstack(inputs)
-
-def subsampleTrainingSet(inputs, targets, nsamples):
-    """
-    Returns a random subset of the training data set given as input.
-    """
-    assert inputs.shape[0] == targets.shape[0]
-    n = inputs.shape[0]
-    assert nsamples <= n
-    permutation = np.random.permutation(n)[:nsamples]
-    return inputs[permutation], targets[permutation]
-
-class DataProcessor(DynamicsLearnerInterface):
-    # Mix-in class to normalize inputs to train predict and (correspondingly) denormalize outputs of predict
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.eps = 1e-8
-        self.mean_states, self.std_states, self.mean_deltas, self.std_deltas, self.mean_acts, self.std_acts = [None] * 6
-
-    def learn(self, observation_sequences, action_sequences):
-        # delta_states = np.zeros_like(observation_sequences)
-        delta_states = observation_sequences[:, 1:, :] - observation_sequences[:, :-1, :]
-        action_sequences = action_sequences[:, :-1, :]
-        observation_sequences = observation_sequences[:,:-1,:]
-        states = observation_sequences.reshape((observation_sequences.shape[0] * observation_sequences.shape[1], observation_sequences.shape[2]))
-        delta_states = delta_states.reshape((delta_states.shape[0] * delta_states.shape[1], delta_states.shape[2]))
-        actions = action_sequences.reshape((action_sequences.shape[0] * action_sequences.shape[1], action_sequences.shape[2]))
-        #TODO: modify actions
-
-        norm_states, norm_actions, norm_deltas = self.normalize(states, actions, delta_states, override_statistics=True)
-
-        super().learn(norm_states, norm_actions, norm_deltas)
-
-    def predict(self, observation_history, action_history, action_future):
-        last_states = observation_history[-1, :].T
-        states = last_states
-        n_step_prediction = np.zeros((action_future.shape[0], observation_history.shape[1]))
-        for i, action in enumerate(action_future):
-            if states.ndim == 1:
-                states = states[None, ...]
-            if action.ndim == 1:
-                action = action[None, ...]
-            norm_states, norm_actions = self.normalize(states, action, override_statistics=False)
-            norm_deltas = super().predict(norm_states, norm_actions)
-            deltas = self.denormalize(norm_deltas)
-            next_states = states + deltas
-            n_step_prediction[i, :] = states + deltas
-            states = next_states
-        return n_step_prediction
-
-        return states + deltas
-
-    def normalize(self, states, actions, delta_states=None, override_statistics=False):
-        if override_statistics:
-            self.mean_states = np.mean(states, axis=0)
-            self.std_states = np.std(states, axis=0)
-            self.mean_acts = np.mean(actions, axis=0)
-            self.std_acts = np.std(actions, axis=0)
-        normalized_states = (states - self.mean_states) / (self.std_states + self.eps)
-        normalized_actions = (actions - self.mean_acts) / (self.std_acts + self.eps)
-
-        if delta_states is not None:
-            if override_statistics:
-                self.mean_deltas = np.mean(delta_states, axis=0)
-                self.std_deltas = np.std(delta_states, axis=0)
-            normalized_deltas = (delta_states - self.mean_deltas) / (self.std_deltas + self.eps)
-            return normalized_states, normalized_actions, normalized_deltas
-        else:
-            return normalized_states, normalized_actions
-
-    def denormalize(self, deltas):
-        return deltas * self.std_deltas + self.mean_deltas
-
-    def save(self, model_dir):
-        if not os.path.exists(model_dir):
-            os.makedirs(model_dir)
-        normalization_stats = [self.mean_states, self.std_states, self.mean_deltas, self.std_deltas, self.mean_acts, self.std_acts]
-        with open(os.path.join(model_dir ,"normalization_stats.pickle"), 'wb') as handle:
-            pickle.dump(normalization_stats, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        super().save(model_dir)
-
-    def load(self, model_dir):
-         with open(os.path.join(model_dir ,"normalization_stats.pickle"), 'rb') as handle:
-             normalization_stats = pickle.load(handle)
-         self.mean_states, self.std_states, self.mean_deltas, self.std_deltas, self.mean_acts, self.std_acts = normalization_stats
-         super().load(model_dir)
-
-def connected_shuffle(list_arrays):
-    random_state = np.random.RandomState(0)
-    n_samples = list_arrays[0].shape[0]
-    numels = np.array([a.shape[0] for a in list_arrays])
-    if not np.all(numels == n_samples):
-        raise ValueError('Different number of elements along axis 0', numels, n_samples)
-    shuffling_indices = random_state.permutation(n_samples)
-    list_arrays = [a[shuffling_indices] for a in list_arrays]
-    return list_arrays
