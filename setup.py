b4df3d152190cdaf6772125bdff3205739053f8f
Diego Agudelo-Espana <dagudelo@tue.mpg.de>
2021-01-25 20:08:33 +0100

    bugfix

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rr-learning/transferable_dynamics_dataset",
-    packages=find_packages(exclude=("tests",)),
+    packages=setuptools.find_packages(exclude=("tests",)),
     install_requires=[
         'numpy',
         'matplotlib',
